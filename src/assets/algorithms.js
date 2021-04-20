eeb1631a6817f18f833c0a475ddf839b816e6abe
Idan Argaman <idanargaman@gmail.com>
2021-04-20 11:24:02 +0300

    Add algorithms skelton + num of pairs sum to k

--- src/assets/algorithms.js
+++ src/assets/algorithms.js
@@ -1,8 +1,68 @@
+/* eslint-disable */
+
+const CodeTypesEnum = {
+  ALGORITHM: "ALGORITHM"
+}
+
+export default [{
+  "categoryId": CodeTypesEnum.ALGORITHM,
+  "title": "Array pairs that sum to a given K",
+  "description": `Given an array, print the pairs that their sum gives a result equals to k.
+    We can find different implementation here: https://www.geeksforgeeks.org/count-pairs-with-given-sum/`,
+  "code": () => {
+    const sampleArray = [1, 5, 3, 3, 2, 2];
+    const k = 6;
+
+    function findPairs(sampleArray, k) {
+      // Map the array, store in the map the index each ke is found at
+      const map = {};
+      for (let i = 0; i < sampleArray.length; i++) {
+        const j = sampleArray[i];
+        if (map[j] !== undefined) {
+          map[j].push(i);
+        } else {
+          map[j] = [i];
+        }
+      }
+
+      // Use the map to find pairs 
+      const pairs = {};
+      Object.keys(map).forEach(key => {
+        // For a given key in the map, does the map contains a target key that complement it to k?
+        if (map[k - key] !== undefined) {
+          // if so iterate to indexes for the given key
+          map[key].forEach(lidx => {
+            // and iterate the indexes for the target key
+            map[k - key].forEach(ridx => {
+              // if the target key is different from the given key
+              // this is important to avoid result for item being added to itself
+              if (ridx !== lidx) {
+                // push the key pairs to the result, using map avoids dup counting
+                const a = Math.min(sampleArray[lidx], sampleArray[ridx]);
+                const b = Math.max(sampleArray[lidx], sampleArray[ridx]);
+                pairs[[a, b]] = pairs[[a, b]] ? pairs[[a, b]] + 1 : 1;
+              }
+            })
+          })
+        }
+      });
+
+      Object.keys(pairs).forEach(keyPair => console.log(keyPair + `: ${pairs[keyPair] / 2}`));
+    }
+
+
+    findPairs(sampleArray, k);
+
+  }
+}]
+
+
+/*
 Kanden
 missing number 1...N - two versions: one that handles large number of items
 smart stack with getMin
 array sum until index + change item
-https://www.geeksforgeeks.org/count-pairs-with-given-sum/
+
 https://www.log2base2.com/data-structures/array/remove-a-specific-element-from-array.html
 
 ------------------------------------------------------------------
@@ -101,4 +161,4 @@ function siftDown(heap, node, i) {
 }
 
 ------------------------------------------------------------------
-
+*/
